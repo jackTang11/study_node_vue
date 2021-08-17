64d4b55136d44f64b2ea4bd02a7c38efc578c796
jack <461897266@qq.com>
2021-08-17 13:18:14 +0800

    fix

---
+++ admin1/src/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from '../views/Main.vue'
+import CategoryEdit from '../views/CategoryEdit.vue'
+import CategoryList from '../views/CategoryList.vue'
+
+import ItemEdit from '../views/ItemEdit.vue'
+import ItemList from '../views/ItemList.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'main',
+    component: Main,
+    children:[
+      {
+        path: '/categories/create',
+        component: CategoryEdit
+      },
+      {
+        path: '/categories/edit/:id',
+        component: CategoryEdit,
+        props: true
+      },
+      {
+        path: '/categories/list',
+        component: CategoryList
+      },
+
+      {
+        path: '/item/create',
+        component: ItemEdit
+      },
+      {
+        path: '/item/edit/:id',
+        component: ItemEdit,
+        props: true
+      },
+      {
+        path: '/item/list',
+        component: ItemList
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
